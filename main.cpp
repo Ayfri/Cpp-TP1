b8003bca8aa486adb3e4a0326c7317ed0fc77ebf
Ayfri <pierre.ayfri@gmail.com>
2022-03-31 16:35:11 +0200

    feat: Add menu.

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "main.hpp"
+
+int main() {
+	// fixe les accents dans les consoles sous windows
+	SetConsoleOutputCP( 65001 );
+	
+	show_menu();
+}
+
+
---
+++ main.hpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <windows.h>
+#include "src/menu.hpp"
+
+#ifndef TP1_MAIN_HPP
+#define TP1_MAIN_HPP
+
+/**
+ * Lancement du programme
+ */
+int main();
+
+#endif //TP1_MAIN_HPP
